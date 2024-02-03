e95e14625d4065db380fae3adff0f9ab186035ea
Artur Reiljan <arreil@taltech.ee>
2024-02-03 16:12:31 +0200

    tere v2

--- EX/EX01IdCode/src/ee/taltech/iti0202/idcode/IdCode.java
+++ EX/EX01IdCode/src/ee/taltech/iti0202/idcode/IdCode.java
@@ -18,9 +18,9 @@ public class IdCode {
     }
     public IdCode(String idCodeValue) {
         this.idCodeValue = idCodeValue;
-        // if (!isCorrect()) {
-        //   throw new IllegalArgumentException();
-        // }
+        if (!isCorrect()) {
+           throw new IllegalArgumentException();
+        }
 
 
     }
