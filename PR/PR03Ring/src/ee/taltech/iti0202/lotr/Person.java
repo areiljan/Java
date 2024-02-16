66f8772cc5ec489759be8892e402add797dc4648
Artur Reiljan <arreil@taltech.ee>
2024-02-16 22:22:06 +0200

    tere v2

--- PR/PR03Ring/src/ee/taltech/iti0202/lotr/Person.java
+++ PR/PR03Ring/src/ee/taltech/iti0202/lotr/Person.java
@@ -5,30 +5,62 @@ public class Person {
     private String name = null;
     private Ring ring = null;
 
+    /**
+     * This method constructs the Person object.
+     *
+     * @param race is a string.
+     * @param name is a string.
+     * @param ring is a Ring object.
+     */
     public Person(String race, String name, Ring ring) {
         this.race = race;
         this.name = name;
         this.ring = ring;
     }
 
+    /**
+     * This method constructs the Person object.
+     *
+     * @param race is a string.
+     * @param name is a string.
+     */
     public Person(String race, String name) {
         this.race = race;
         this.name = name;
     }
 
+    /**
+     * This method assigns a ring to the Person object.
+     *
+     * @param ring is a Ring object.
+     */
     public void setRing(Ring ring) {
         this.ring = ring;
     }
 
+    /**
+     * Is the given Person object with the assigned ring sauron or not.
+     *
+     @return The result of the operation. Possible return values:
+      *         <ul>
+      *             <li>returns "Affirmative"</li>
+      *             <li>returns "No, the ring is fake!"</li>
+      *             <li>returns "No, but he's claiming to be"</li>
+      *             <li>returns "No. he just stole the ring"</li>
+      *             <li>returns "No"</li>
+      *         </ul>
+     */
     public String isSauron() {
         if (ring != null) {
-            if (this.name.equals("Sauron") && this.ring.getType().equals(Ring.Type.THE_ONE) && this.ring.getMaterial().equals(Ring.Material.GOLD)) {
+            if (this.name.equals("Sauron") && this.ring.getMaterial().equals(Ring.Material.GOLD)
+                    && this.ring.getType().equals(Ring.Type.THE_ONE)) {
                 return "Affirmative";
             } else if (this.name.equals("Sauron") && this.ring.getType().equals(Ring.Type.THE_ONE)) {
                 return "No, the ring is fake!";
             } else if (this.name.equals("Sauron")) {
                 return "No, but he's claiming to be";
-            } else if (this.ring.getType().equals(Ring.Type.THE_ONE) && this.ring.getMaterial().equals(Ring.Material.GOLD)) {
+            } else if (this.ring.getType().equals(Ring.Type.THE_ONE)
+                    && this.ring.getMaterial().equals(Ring.Material.GOLD)) {
                 return "No, he just stole the ring";
             } else {
                 return "No";
--- PR/PR03Ring/src/ee/taltech/iti0202/lotr/Ring.java
+++ PR/PR03Ring/src/ee/taltech/iti0202/lotr/Ring.java
@@ -4,13 +4,31 @@ public class Ring {
     private Type type;
     private Material material;
 
+    /**
+     * This method constructs the ring object.
+     *
+     * @param type The first object.
+     * @param material The second object.
+     */
     public Ring(Type type, Material material) {
         this.type = type;
         this.material = material;
     }
+
+    /**
+     * This method returns the type of the given ring.
+     *
+     * @return type.
+     */
     public Type getType() {
         return type;
     }
+
+    /**
+     * This method returns the material of the given ring.
+     *
+     * @return material.
+     */
     public Material getMaterial() {
         return material;
     }
