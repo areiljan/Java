7f71da46c255037decdadddc98828283b5e81b79
Artur Reiljan <arreil@taltech.ee>
2024-03-17 15:04:15 +0200

    tere v2

--- TK/TK3/src/ee/taltech/iti0202/tk/Exam.java
+++ TK/TK3/src/ee/taltech/iti0202/tk/Exam.java
@@ -1,6 +1,10 @@
 package ee.taltech.iti0202.tk;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.Map;
 
 public class Exam {
 
