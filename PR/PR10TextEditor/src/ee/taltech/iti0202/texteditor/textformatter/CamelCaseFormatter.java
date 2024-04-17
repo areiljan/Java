89a509c67e8d99a3dde2630ad022efccdff8a41b
Artur Reiljan <arreil@taltech.ee>
2024-04-18 00:38:51 +0300

    PR10 14th push - fixing.

--- PR/PR10TextEditor/src/ee/taltech/iti0202/texteditor/textformatter/CamelCaseFormatter.java
+++ PR/PR10TextEditor/src/ee/taltech/iti0202/texteditor/textformatter/CamelCaseFormatter.java
@@ -4,10 +4,6 @@ public class CamelCaseFormatter implements TextFormatter {
 
     @Override
     public String format(String text) {
-        if (text == null || text.isEmpty()) {
-            return "";
-        }
-
         String[] words = text.split("[^\\p{Alnum}]+");
         StringBuilder camelCaseString = new StringBuilder();
         for (int i = 0; i < words.length; i++) {
@@ -18,16 +14,22 @@ public class CamelCaseFormatter implements TextFormatter {
             if (i == 0) {
                 camelCaseString.append(word.toLowerCase());
             } else {
-                boolean retainPreviousPunctuation = false;
-                if (i > 1 && word.length() >= 2 && word.charAt(word.length() - 2) == '\n') {
-                    retainPreviousPunctuation = true;
+                // Check if the previous character is a symbol and should be retained
+                boolean retainPreviousSymbol = false;
+                if (i > 1 && word.length() >= 2) {
+                    char prevChar = word.charAt(word.length() - 2);
+                    char lastChar = word.charAt(word.length() - 1);
+                    if ((prevChar == '\n' || !Character.isLetterOrDigit(prevChar)) && (Character.isLetterOrDigit(lastChar) || lastChar == '\n')) {
+                        retainPreviousSymbol = true;
+                    }
                 } else if (i > 0 && word.length() >= 1) {
                     char prevChar = words[i - 1].charAt(words[i - 1].length() - 1);
-                    if (!Character.isLetterOrDigit(prevChar)) {
-                        retainPreviousPunctuation = true;
+                    if (!Character.isLetterOrDigit(prevChar) && prevChar != '.' && prevChar != '\n') {
+                        retainPreviousSymbol = true;
                     }
                 }
-                camelCaseString.append(retainPreviousPunctuation ? word.substring(0, word.length() - 2) : word.substring(0, 1).toUpperCase())
+                // Append the current word in camel case format
+                camelCaseString.append(retainPreviousSymbol ? word.substring(0, word.length() - 1) : word.substring(0, 1).toUpperCase())
                         .append(word.substring(1).toLowerCase());
             }
         }
