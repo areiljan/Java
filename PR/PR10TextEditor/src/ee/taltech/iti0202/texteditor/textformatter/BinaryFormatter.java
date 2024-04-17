4d7bfa20ada69556f3255b1dc79ba9f3c7f5f172
Artur Reiljan <arreil@taltech.ee>
2024-04-18 00:31:57 +0300

    PR10 13th push - fixing.

--- PR/PR10TextEditor/src/ee/taltech/iti0202/texteditor/textformatter/BinaryFormatter.java
+++ PR/PR10TextEditor/src/ee/taltech/iti0202/texteditor/textformatter/BinaryFormatter.java
@@ -3,21 +3,15 @@ package ee.taltech.iti0202.texteditor.textformatter;
 public class BinaryFormatter implements TextFormatter {
     @Override
     public String format(String text) {
-        StringBuilder binaryString = new StringBuilder();
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            String binary = Integer.toBinaryString(c);
-
-            while (binary.length() < 8) {
-                binary = "0" + binary;
-            }
-
-            binaryString.append(binary);
-
-            if (i < text.length() - 1 && !(c == '\n' && text.charAt(i + 1) != '\r')) {
-                binaryString.append(" ");
-            }
+        if (text == null) {
+            // Handle null input gracefully
+            return "";
         }
-        return binaryString.toString();
+
+        StringBuilder binaryString = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            binaryString.append(Integer.toBinaryString(c)).append(" ");
+        }
+        return binaryString.toString().trim();
     }
 }
