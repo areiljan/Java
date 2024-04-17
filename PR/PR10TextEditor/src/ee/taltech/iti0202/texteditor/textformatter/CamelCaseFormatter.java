025f6cd71523b838c6e77e0724401367e2862db1
Artur Reiljan <arreil@taltech.ee>
2024-04-18 00:34:41 +0300

    PR10 14th push - fixing.

--- PR/PR10TextEditor/src/ee/taltech/iti0202/texteditor/textformatter/CamelCaseFormatter.java
+++ PR/PR10TextEditor/src/ee/taltech/iti0202/texteditor/textformatter/CamelCaseFormatter.java
@@ -4,14 +4,31 @@ public class CamelCaseFormatter implements TextFormatter {
 
     @Override
     public String format(String text) {
-        String[] words = text.replaceAll("[^\\w\\s]|_", "").split("\\s+");
+        if (text == null || text.isEmpty()) {
+            return "";
+        }
+
+        String[] words = text.split("[^\\p{Alnum}]+");
         StringBuilder camelCaseString = new StringBuilder();
         for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (word.isEmpty()) {
+                continue; // Skip empty strings
+            }
             if (i == 0) {
-                camelCaseString.append(words[i].toLowerCase());
+                camelCaseString.append(word.toLowerCase());
             } else {
-                camelCaseString.append(words[i].substring(0, 1).toUpperCase())
-                        .append(words[i].substring(1).toLowerCase());
+                boolean retainPreviousPunctuation = false;
+                if (i > 1 && word.length() >= 2 && word.charAt(word.length() - 2) == '\n') {
+                    retainPreviousPunctuation = true;
+                } else if (i > 0 && word.length() >= 1) {
+                    char prevChar = words[i - 1].charAt(words[i - 1].length() - 1);
+                    if (!Character.isLetterOrDigit(prevChar)) {
+                        retainPreviousPunctuation = true;
+                    }
+                }
+                camelCaseString.append(retainPreviousPunctuation ? word.substring(0, word.length() - 2) : word.substring(0, 1).toUpperCase())
+                        .append(word.substring(1).toLowerCase());
             }
         }
         return camelCaseString.toString();
