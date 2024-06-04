b041cbfbf212daea38103c3bb28903bfe67582fb
arreil <arreil@taltech.ee>
2024-06-04 15:53:26 +0300

    exam

--- EXAM/OOP4/src/ee/taltech/iti0202/exam/dotkomm/DotKomm.java
+++ EXAM/OOP4/src/ee/taltech/iti0202/exam/dotkomm/DotKomm.java
@@ -12,8 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static java.util.Collections.sort;
-
 public class DotKomm {
 
     private List<Product> productList;
