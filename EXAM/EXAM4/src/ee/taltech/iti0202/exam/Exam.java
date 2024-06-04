cd279f33aef93696e5c966f0f3016b2f8a33bef8
arreil <arreil@taltech.ee>
2024-06-04 13:06:41 +0300

    exam

--- EXAM/EXAM4/src/ee/taltech/iti0202/exam/Exam.java
+++ EXAM/EXAM4/src/ee/taltech/iti0202/exam/Exam.java
@@ -56,15 +56,14 @@ public class Exam {
         String[] time2List = time2.split(":");
         Integer hours = Integer.parseInt(time2List[0]) - Integer.parseInt(time1List[0]);
         Integer minutes = Integer.parseInt(time2List[1]) - Integer.parseInt(time1List[1]);
-        if (hours < 0) {
+        if (hours == 0 && minutes < 0) {
+            hours -= 1;
+        } if (hours < 0) {
             hours += 24;
         }
 
         if (minutes < 0) {
             minutes += 60;
-            if (hours == 0) {
-                hours = 23;
-            }
         }
         String hoursAsString = "";
         String minutesAsString = "";
