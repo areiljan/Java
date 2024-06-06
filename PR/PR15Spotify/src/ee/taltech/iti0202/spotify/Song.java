ad1bd65090dcb11cd06af6d64ce01f1ea1c3ff00
Artur Reiljan <arreil@taltech.ee>
2024-06-06 14:33:15 +0300

    EX15 - Spotify app

--- PR/PR15Spotify/src/ee/taltech/iti0202/spotify/Song.java
+++ PR/PR15Spotify/src/ee/taltech/iti0202/spotify/Song.java
@@ -5,6 +5,6 @@ public record Song(String title, String artist, int duration) {
     @Override
     public String toString() {
         // TODO
-        return title + " by " + artist;
+        return "\"" + title + "\"" + " by " + artist;
     }
 }
