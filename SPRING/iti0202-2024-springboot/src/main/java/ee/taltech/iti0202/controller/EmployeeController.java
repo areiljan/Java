462523eb919a3873d53034d938f49a2ae30971be
Artur Reiljan <arreil@taltech.ee>
2024-06-07 13:28:12 +0300

    Spring boot.

--- SPRING/iti0202-2024-springboot/src/main/java/ee/taltech/iti0202/controller/EmployeeController.java
+++ SPRING/iti0202-2024-springboot/src/main/java/ee/taltech/iti0202/controller/EmployeeController.java
@@ -5,14 +5,26 @@ import ee.taltech.iti0202.repository.Employee;
 import ee.taltech.iti0202.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * The type Employee controller.
+ */
 @RequiredArgsConstructor
 @RestController
 public class EmployeeController {
+    public static final int STATUS_URL_NOT_FOUND = 404;
+    public static final int STATUS_SOMETHING_WRONG_SERVERSIDE = 500;
     private final EmployeeService employeeService;
 
     // @GetMapping("/employees")
@@ -20,6 +32,12 @@ public class EmployeeController {
     //     return employeeService.findAllEmployees();
     // }
 
+    /**
+     * Gets employee by id.
+     *
+     * @param id the id
+     * @return the employee by id
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Employee>> getEmployeeById(@PathVariable Long id) {
         Optional<Employee> employee = employeeService.findEmployeeById(id);
@@ -30,6 +48,12 @@ public class EmployeeController {
         }
     }
 
+    /**
+     * Add employee response entity.
+     *
+     * @param employee the employee
+     * @return the response entity
+     */
     @PostMapping("/add")
     public ResponseEntity<String> addEmployee(@RequestBody Employee employee) {
         if (employeeService.isEmailAlreadyTaken(employee.getEmail())) {
@@ -38,32 +62,51 @@ public class EmployeeController {
             if (employeeService.addEmployee(employee)) {
                 return ResponseEntity.ok("Employee added to database.");
             } else {
-                return ResponseEntity.status(500).body("Something went wrong.");
+                return ResponseEntity.status(STATUS_SOMETHING_WRONG_SERVERSIDE).body("Something went wrong.");
             }
         }
     }
 
+    /**
+     * Delete employee response entity.
+     *
+     * @param id the id
+     * @return the response entity
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteEmployee(@PathVariable long id) {
         if (employeeService.deleteEmployeeById(id)) {
             return ResponseEntity.ok("Employee deleted");
         } else {
-            return ResponseEntity.status(404).body("No matching ID found in database!");
+            return ResponseEntity.status(STATUS_URL_NOT_FOUND).body("No matching ID found in database!");
         }
     }
 
+    /**
+     * Update employee response entity.
+     *
+     * @param id              the id
+     * @param updatedEmployee the updated employee
+     * @return the response entity
+     */
     @PutMapping("/{id}")
     public ResponseEntity<String> updateEmployee(@PathVariable long id, @RequestBody Employee updatedEmployee) {
         String result = employeeService.updateEmployee(id, updatedEmployee);
         if ("Cannot overwrite employee data!".equals(result)) {
             return ResponseEntity.badRequest().body(result);
         } else if ("Employee not found!".equals(result)) {
-            return ResponseEntity.status(404).body(result);
+            return ResponseEntity.status(STATUS_URL_NOT_FOUND).body(result);
         } else {
             return ResponseEntity.ok(result);
         }
     }
 
+    /**
+     * Gets employees by company.
+     *
+     * @param company the company
+     * @return the employees by company
+     */
     @GetMapping("/employees")
     public List<Employee> getEmployeesByCompany(@RequestParam(name = "company", required = false) String company) {
         if (company == null || company.isEmpty() || company.isBlank()) {
--- SPRING/iti0202-2024-springboot/src/main/java/ee/taltech/iti0202/service/EmployeeService.java
+++ SPRING/iti0202-2024-springboot/src/main/java/ee/taltech/iti0202/service/EmployeeService.java
@@ -8,24 +8,50 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@RequiredArgsConstructor // loob konstuktori ise parameetriga EmployeeRepository 
+/**
+ * The type Employee service.
+ */
+@RequiredArgsConstructor
 @Service
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
+    /**
+     * Is email already taken boolean.
+     *
+     * @param email the email
+     * @return the boolean
+     */
     public boolean isEmailAlreadyTaken(String email) {
         return employeeRepository.existsByEmail(email);
     }
 
+    /**
+     * Find all employees list.
+     *
+     * @return the list
+     */
     public List<Employee> findAllEmployees() {
         return employeeRepository.findAll();
     }
 
+    /**
+     * Find employee by id optional.
+     *
+     * @param id the id
+     * @return the optional
+     */
     public Optional<Employee> findEmployeeById(long id) {
         return employeeRepository.findById(id);
     }
 
+    /**
+     * Add employee boolean.
+     *
+     * @param employee the employee
+     * @return the boolean
+     */
     public boolean addEmployee(Employee employee) {
         try {
             employeeRepository.save(employee);
@@ -36,6 +62,12 @@ public class EmployeeService {
         }
     }
 
+    /**
+     * Delete employee by id boolean.
+     *
+     * @param id the id
+     * @return the boolean
+     */
     public boolean deleteEmployeeById(long id) {
         if (employeeRepository.existsById(id)) {
             employeeRepository.deleteById(id);
@@ -45,6 +77,13 @@ public class EmployeeService {
         }
     }
 
+    /**
+     * Update employee string.
+     *
+     * @param id              the id
+     * @param updatedEmployee the updated employee
+     * @return the string
+     */
     public String updateEmployee(long id, Employee updatedEmployee) {
         Optional<Employee> existingEmployeeOptional = employeeRepository.findById(id);
         if (existingEmployeeOptional.isPresent()) {
@@ -63,6 +102,12 @@ public class EmployeeService {
         return "Employee not found!";
     }
 
+    /**
+     * Gets employees by company.
+     *
+     * @param company the company
+     * @return the employees by company
+     */
     public List<Employee> getEmployeesByCompany(String company) {
         return employeeRepository.findByCompany(company);
     }
